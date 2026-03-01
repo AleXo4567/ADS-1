1162bd0e980f01aa7688693196b97d8f149ff5af
Alex <citilink25818@gmail.com>
2026-03-01 21:41:43 +0300

    Update alg.cpp
    вапвап

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,16 +1,16 @@
-// Copyright 2022 NNTU-CS
+// Copyright 2022 NNTU
 #include <cstdint>
 #include <cmath>
 #include <iostream>
 #include "alg.h"
 
 
-bool checkPrime(uint64_t value) {
+bool CheckPrime(uint64_t value) {
     if (value <= 1) {
         return false;
     }
 
-    for (uint64_t i = 2; i * i <= value; i++) {
+    for (uint64_t i = 2; i * i <= value; ++i) {
         if (value % i == 0) {
             return false;
         }
@@ -19,7 +19,7 @@ bool checkPrime(uint64_t value) {
     return true;
 }
 
-uint64_t nPrime(uint64_t n) {
+uint64_t NPrime(uint64_t n) {
     if (n == 0) {
         return 0;
     }
@@ -28,34 +28,34 @@ uint64_t nPrime(uint64_t n) {
     uint64_t number = 2;
 
     while (count < n) {
-        if (checkPrime(number)) {
-            count++;
+        if (CheckPrime(number)) {
+            ++count;
             if (count == n) {
                 return number;
             }
         }
-        number++;
+        ++number;
     }
 
     return 0;
 }
 
-uint64_t nextPrime(uint64_t value) {
+uint64_t NextPrime(uint64_t value) {
     uint64_t number = value + 1;
 
     while (true) {
-        if (checkPrime(number)) {
+        if (CheckPrime(number)) {
             return number;
         }
-        number++;
+        ++number;
     }
 }
 
-uint64_t sumPrime(uint64_t hbound) {
+uint64_t SumPrime(uint64_t hbound) {
     uint64_t sum = 0;
 
-    for (uint64_t i = 2; i < hbound; i++) {
-        if (checkPrime(i)) {
+    for (uint64_t i = 2; i < hbound; ++i) {
+        if (CheckPrime(i)) {
             sum += i;
         }
     }
@@ -63,13 +63,13 @@ uint64_t sumPrime(uint64_t hbound) {
     return sum;
 }
 
-uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
+uint64_t TwinPrimes(uint64_t lbound, uint64_t hbound) {
     uint64_t count = 0;
 
-    for (uint64_t i = lbound; i < hbound - 1; i++) {
-        if (checkPrime(i) && checkPrime(i + 2)) {
+    for (uint64_t i = lbound; i < hbound - 1; ++i) {
+        if (CheckPrime(i) && CheckPrime(i + 2)) {
             if (i >= lbound && i + 2 < hbound) {
-                count++;
+                ++count;
             }
         }
     }
