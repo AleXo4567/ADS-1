42ae0cc4c639841f8c63b18ca444ee9d58ccaffc
Alex <citilink25818@gmail.com>
2026-03-01 21:27:26 +0300

    Update alg.cpp
    okel

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,5 +1,7 @@
 // Copyright 2022 NNTU-CS
 #include <cstdint>
+#include <cmath>
+#include <iostream>
 #include "alg.h"
 
 
@@ -10,26 +12,26 @@ bool checkPrime(uint64_t value) {
 
     for (uint64_t i = 2; i * i <= value; i++) {
         if (value % i == 0) {
-            return false; 
+            return false;
         }
     }
 
-    return true; 
+    return true;
 }
 
 uint64_t nPrime(uint64_t n) {
     if (n == 0) {
-        return 0; 
+        return 0;
     }
 
-    uint64_t count = 0; 
-    uint64_t number = 2; 
+    uint64_t count = 0;
+    uint64_t number = 2;
 
     while (count < n) {
         if (checkPrime(number)) {
-            count++; 
+            count++;
             if (count == n) {
-                return number; 
+                return number;
             }
         }
         number++;
@@ -39,7 +41,7 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-    uint64_t number = value + 1; 
+    uint64_t number = value + 1;
 
     while (true) {
         if (checkPrime(number)) {
@@ -54,7 +56,7 @@ uint64_t sumPrime(uint64_t hbound) {
 
     for (uint64_t i = 2; i < hbound; i++) {
         if (checkPrime(i)) {
-            sum += i; 
+            sum += i;
         }
     }
 
